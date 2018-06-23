9c312de72162921fab0fcf733b9ee9086005124d
Soumya <soumyaasgoku@gmail.com>
2018-06-23 17:57:46 +0530

    Read Trees

---
+++ geekforgeeks/trees/BST/delete.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *right,*left;
+};
+
+Node* newNode(int data){
+    Node* temp=new Node;
+    temp->data=data;
+    temp->right=NULL;
+    temp->left=NULL;
+    return temp;
+}
+
+void inorder(Node* temp){
+    if(!temp)
+        return;
+    inorder(temp->left);
+    cout<<temp->data<<" ";
+    inorder(temp->right);
+}
+
+Node* insert(Node* temp,int data){
+    if(temp == NULL)
+        return newNode(data);
+    if(data>temp->data)
+        temp->right=insert(temp->right,data);
+    if(data<temp->data)
+        temp->left=insert(temp->left,data);
+    return temp;
+}
+
+Node* minValueNode(Node* temp){
+    Node* current=temp;
+    while(current->left != NULL){
+        current= current->left;
+    }
+    return current;
+}
+
+Node* deleteNode(Node* root,int data){
+    if(root== NULL)
+        return root;
+    if(data > root->data)
+        root->right = deleteNode(root->right,data);
+    else if(data < root->data)
+        root->left = deleteNode(root->left,data);
+    else
+    {
+        if(root->left == NULL){
+            Node* temp=root->right;
+            free(root);
+            return temp;
+        }
+        else if(root->right == NULL){
+            Node* temp=root->left;
+            free(root);
+            return temp;
+        }
+
+        Node *temp=minValueNode(root->right);
+        root->data=temp->data;
+        root->right=deleteNode(root->right,temp->data);
+    }
+    return root;
+}
+
+int main()
+{
+    Node *root = NULL;
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+ 
+    printf("Inorder traversal of the given tree \n");
+    inorder(root);
+ 
+    printf("\nDelete 20\n");
+    root = deleteNode(root, 20);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
+ 
+    printf("\nDelete 30\n");
+    root = deleteNode(root, 30);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
+ 
+    printf("\nDelete 50\n");
+    root = deleteNode(root, 50);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
+ 
+    return 0;
+}
---
+++ geekforgeeks/trees/BST/insert.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *right,*left;
+};
+
+Node* newNode(int data){
+    Node* temp=new Node;
+    temp->data=data;
+    temp->right=NULL;
+    temp->left=NULL;
+    return temp;
+}
+
+void inorder(Node* temp){
+    if(!temp)
+        return;
+    inorder(temp->left);
+    cout<<temp->data<<" ";
+    inorder(temp->right);
+}
+
+Node* insert(Node* temp,int data){
+    if(temp == NULL)
+        return newNode(data);
+    if(data>temp->data)
+        temp->right=insert(temp->right,data);
+    if(data<temp->data)
+        temp->left=insert(temp->left,data);
+    return temp;
+}
+
+int main()
+{
+    Node *root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+  
+    // print inoder traversal of the BST
+    inorder(root);
+  
+    return 0;
+}
